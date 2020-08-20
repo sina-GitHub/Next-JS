977f8c94af9c9df47a9358ff0ca310e23d40c79f
SinaJS <54321511+sina-GitHub@users.noreply.github.com>
2020-08-20 11:27:20 -0400

    Add files via upload

---
+++ pages/Home.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+export default function Home2() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Free Web tutorials" />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="author" content="sina" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
+
+      <main>
+        <h1 className="title">Home</h1>
+      </main>
+    </div>
+  );
+}
